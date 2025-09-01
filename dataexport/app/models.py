b302dbaf548a0f98c6e13d0f576729e2e51d750c
Vinicius Begosso Pereira <37528824+ViniciusBPereira@users.noreply.github.com>
2025-09-01 00:17:00 -0300

    Update models.py

--- dataexport/app/models.py
+++ dataexport/app/models.py
@@ -3,14 +3,17 @@ from typing import Optional
 
 class Tarefa(BaseModel):
     Id: str
+    Nome: str
     Local: str
     TerminoReal: str
 
 class Checklist(BaseModel):
     tarefaId: str
+    nome: str
     local: str
     data: str
     recursonome: Optional[str]
     tipo: Optional[int]
     perguntadescricao: Optional[str]
+
     conteudo: Optional[str]
